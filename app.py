583337c55b90aa5be65427af0a2ff33f8f34b22f
Michael Ruvinshteyn <mruvinshteyn@stuy.edu>
2017-11-09 13:01:40 -0500

    added app

---
+++ app.py
@@ -0,0 +1,13 @@
+import urllib2
+
+url = "https://api.nasa.gov/planetary/apod?api_key=2GTxH8djyXY3fq4DXGy5nY7o6OPAZq3oAhq53opG"
+site = urllib2.urlopen(url)
+s = site.read()
+s_split = s.split('\n')
+s_dict = []
+for i in s_split:
+    s_dict.append(i.split(':'))
+d = {}
+for i in s_dict:
+    if len(i) == 2:
+        d[i[0]] = i[1]
