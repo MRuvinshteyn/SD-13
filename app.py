ff052bd1e18e2d44fc7b7d6a414f50f76a613619
Michael Ruvinshteyn <mruvinshteyn@stuy.edu>
2017-11-09 13:12:09 -0500

    modified app

--- app.py
+++ app.py
@@ -6,8 +6,10 @@ s = site.read()
 s_split = s.split('\n')
 s_dict = []
 for i in s_split:
-    s_dict.append(i.split(':'))
+    s_dict.append(i.split(':',1))
 d = {}
 for i in s_dict:
-    if len(i) == 2:
+    if len(i) >= 2:
         d[i[0]] = i[1]
+
+
